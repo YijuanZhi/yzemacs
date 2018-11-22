9a40291152ec477d9473c93d691adb4c97c53f0b
Yijuan Zhi <zjzyj520@gmail.com>
2018-11-22 06:57:20 -0500

    new update

--- init.el
+++ init.el
@@ -38,6 +38,8 @@
 		      exec-path-from-shell
 		      ;; --- Themes ---
 		      zenburn-theme
+		      monokai-theme
+		      sublime-themes
 		      ) "Default packages")
 
 (setq package-selected-packages my/packages)
@@ -134,6 +136,9 @@
  ;; If there is more than one, they won't work right.
  '(company-idle-delay 0.1)
  '(company-minimum-prefix-length 1)
+ '(custom-safe-themes
+   (quote
+    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "f66ffeadda7b52d40c8d698967ae9e9836f54324445af95610d257fa5e3e1e21" "84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
  '(package-archives
    (quote
     (("gnu" . "http://elpa.gnu.org/packages/")
