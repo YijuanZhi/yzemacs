1984a7e3d20d24bef7adfe1ceb990ef7f79e819c
Zac <zjzyj520@gmail.com>
2018-11-21 19:31:38 -0500

    first commit

---
+++ init.el
@@ -0,0 +1,188 @@
+;;  Use f2 to open init.el file
+(defun open-my-init-file()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+
+(global-set-key (kbd "<f2>") 'open-my-init-file)
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+
+
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (package-initialize)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+  )
+
+;; cl - Common Lisp Extension
+(require 'cl)
+
+;; Add Packages
+(defvar my/packages '(
+		      ;; --- Auto-completion ---
+		      company
+		      ;; --- Better Editor ---
+		      smooth-scrolling
+		      hungry-delete
+		      swiper
+		      counsel
+		      smartparens
+		      exec-path-from-shell
+		      ;; --- Major Mode ---
+		      js2-mode
+		      ;; --- Minor Mode ---
+		      nodejs-repl
+		      exec-path-from-shell
+		      ;; --- Themes ---
+		      zenburn-theme
+		      ) "Default packages")
+
+(setq package-selected-packages my/packages)
+
+(defun my/packages-installed-p ()
+  (loop for pkg in my/packages
+	when (not (package-installed-p pkg)) do (return nil)
+	finally (return t)))
+
+(unless (my/packages-installed-p)
+  (message "%s" "Refreshing package database...")
+  (package-refresh-contents)
+  (dolist (pkg my/packages)
+    (when (not (package-installed-p pkg))
+      (package-install pkg))))
+
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize)) ;; Find Executable Path on OS X
+
+
+
+;;------------------------------------------------------------------------------
+
+
+;; smooth-scrolling mode on
+(require 'smooth-scrolling)
+(smooth-scrolling-mode 1)
+
+
+;; configure for nodejs(running js files)
+(require 'nodejs-repl)
+(add-hook 'js-mode-hook
+          (lambda ()
+            (define-key js-mode-map (kbd "C-x C-e") 'nodejs-repl-send-buffer)))
+
+
+
+;; configure for js2 mode
+(require 'js2-mode)
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+;; Better imenu
+(add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
+
+
+;; configure for smartparens
+(require 'smartparens-config) ;; Always start smartparens mode in js-mode.
+(smartparens-global-mode t)
+	  
+
+;; configure for swiper
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(setq enable-recursive-minibuffers t)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "C-h f") 'counsel-describe-function)
+(global-set-key (kbd "C-h v") 'counsel-describe-variable)
+
+
+;; hungry delete t
+(require 'hungry-delete)
+(global-hungry-delete-mode)
+
+;; bar mode
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(setq-default cursor-type 'bar)
+
+;; (electric-indent-mode -1)
+(global-linum-mode t)
+(setq inhibit-splash-screen t)
+
+;; company mode ready
+(global-company-mode t)
+
+;; back up disabled
+(setq make-backup-files nil)
+
+
+(require 'recentf)
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-idle-delay 0.1)
+ '(company-minimum-prefix-length 1)
+ '(package-archives
+   (quote
+    (("gnu" . "http://elpa.gnu.org/packages/")
+     ("melpa" . "http://melpa.org/packages/"))))
+ '(package-selected-packages (quote (gruvbox-theme zenburn-theme dracula-theme company)))
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Hack" :foundry "outline" :slant normal :weight normal :height 120 :width normal))))
+ '(js2-external-variable ((t (:foreground "dark gray")))))
+
+(require 'recentf)
+(recentf-mode 1)
+(setq recentf-max-menu-item 10)
+
+;; delete selection
+(delete-selection-mode 1)
+
+;; initial frame
+(setq initial-frame-alist (quote ((fullscreen . maximized))))
+
+;; show hook
+(add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
+
+
+(global-hl-line-mode 1)
+
+;; theme setting
+;; zenburn, monokai, dracula
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(load-theme 'dracula t)
+
+;; org source code key word highlight
+(require 'org)
+(setq org-src-fontify-natively t)
+
+;; 设置默认 Org Agenda 文件目录
+(setq org-agenda-files '("~/org"))
+;; 设置 org-agenda 打开快捷键
+(global-set-key (kbd "C-c a") 'org-agenda)
+
+;; default font set
+(set-face-attribute 'default nil
+                    :family "Hack"
+                    :height 140
+                    :weight 'normal
+                    :width 'normal)
+
+;; turn off alarm sound (noice!)
+(setq ring-bell-function 'ignore)
